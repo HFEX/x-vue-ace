d9130cfb8a23a74cf3a1babec02ba5f0b812076b
YuAoi <zibo.z.liu@gmail.com>
2020-05-29 23:49:54 +0800

    fix: 修复插入代码对齐异常的问题；

--- package/XVueAce.vue
+++ package/XVueAce.vue
@@ -1046,17 +1046,44 @@ export default {
       if (focus) this.editor.focus();
     },
 
-    insertAndSelect(text, pos = '', focus = true) {
+    insertAndSelect(txt, pos = '', focus = true) {
+      const { start } = this.editor.getSelection().getRange();
+      const currLine = this.editor.getSession().getDocument().getLine(start.row);
+      const m = currLine.match(/^\s*\t*/);
+
+      let text = txt;
+      // 如果当前行存在缩进，则在要插入的代码第二行及之后都加上缩进
+      if (m) {
+        const indent = m[0];
+        text = text.replace(/^/gm, (match, p) => {
+          if (p > 0) return `${indent}${match}`;
+          return match;
+        });
+      }
       if (pos) {
         const posArr = pos
           .split(/,|，/)
           .map(v => parseInt(v, 10));
 
-        const { start } = this.editor.getSelection().getRange();
         this.insert(text, focus);
+        const p1 = posArr[0] - 1 || 0;
+        const r = start.row + p1;
+        let c = (
+          posArr[0] > 1
+            ? (posArr[1] || 0)
+            : (posArr[1] || 0) + start.column
+        );
+        // 如果存在缩进&&要选中的是第二行及之后，则加上缩进的位移
+        if (
+          m
+          && p1 > 0
+        ) {
+          c += m[0].length;
+        }
+
         this.select(
-          start.row + (posArr[0] - 1 || 0),
-          posArr[0] > 1 ? (posArr[1] || 0) : start.column + (posArr[1] || 0),
+          r,
+          c,
           posArr[2],
           focus,
         );
